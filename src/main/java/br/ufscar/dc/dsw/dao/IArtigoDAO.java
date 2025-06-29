7df014756a4d22d11f1036bf396a8fc8974a5cb7
Laura Pessine <laurapessine@outlook.com>
2025-06-29 11:43:03 -0300

    Refactor IArtigoDAO and IProfessorDAO interfaces to implement CRUD methods and improve code structure

--- src/main/java/br/ufscar/dc/dsw/dao/IArtigoDAO.java
+++ src/main/java/br/ufscar/dc/dsw/dao/IArtigoDAO.java
@@ -1,5 +1,18 @@
 package br.ufscar.dc.dsw.dao;
 
-public class IArtigoDAO {
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import br.ufscar.dc.dsw.domain.Artigo;
+
+@SuppressWarnings("unchecked")
+public interface IArtigoDAO extends CrudRepository<Artigo, Long> {
     
+    // Métodos de CRUD básicos
+    Artigo findById(long id);
+    List<Artigo> findAll();
+    Artigo save(Artigo artigo);
+    void deleteById(Long id);
+
+    // Métodos de busca personalizados (opcionais, mas úteis)
+    // List<Artigo> findAllByAutores(Professor autor);
 }
--- src/main/java/br/ufscar/dc/dsw/dao/IProfessorDAO.java
+++ src/main/java/br/ufscar/dc/dsw/dao/IProfessorDAO.java
@@ -6,18 +6,9 @@ import br.ufscar.dc.dsw.domain.Professor;
 
 @SuppressWarnings("unchecked")
 public interface IProfessorDAO extends CrudRepository<Professor, Long> {
-    
+    // Métodos de CRUD básicos
     Professor findById(long id);
-
     List<Professor> findAll();
-
     Professor save(Professor professor);
-
     void deleteById(Long id);
-
-    // Métodos de busca personalizados (opcionais, mas úteis)
-
-    // Professor findByEmail(String email);
-    
-    // Professor findByLattes(String lattes);
 }
